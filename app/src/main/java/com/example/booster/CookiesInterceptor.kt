b8f93c676175a87b42e6e3d39ec9d280b2494361
yejin <jinee0717@daum.net>
2020-07-02 22:05:53 +0900

    추가세팅

---
+++ app/src/main/java/com/example/booster/CookiesInterceptor.kt
@@ -0,0 +1,13 @@
+package com.example.booster
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class CookiesInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request =
+            chain.request().newBuilder().header("Content-Type", "application/json")
+                .build()
+        return chain.proceed(request)
+    }
+}
