5a74ea144e1f8a349bf3f4a39c090d75b55ed5b8
KCY <ghkdua059@gmail.com>
2020-07-07 19:50:29 +0900

    [색깔 바꾸기] 고치는중

--- app/src/main/java/com/example/booster/ui/storeList/StoreListFragment.kt
+++ app/src/main/java/com/example/booster/ui/storeList/StoreListFragment.kt
@@ -1,19 +1,17 @@
 package com.example.booster.ui.storeList
 
 import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.a2nd_seminar.ui.ItemDecorator
-
 import com.example.booster.R
-import com.example.booster.ui.MainActivity
 import com.example.booster.ui.storeDetail.StoreDetailActivity
 import kotlinx.android.synthetic.main.fragment_store_list.*
 
@@ -33,7 +31,8 @@ class StoreListFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(StoreListViewModel::class.java)
+//        viewModel = ViewModelProviders.of(this).get(StoreListViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(StoreListViewModel::class.java)
         initRv()
         setClick()
     }
