2065cd5bde2f099d5fa20ecfb01eeebbd23b7366
yejin <jinee0717@daum.net>
2020-07-08 02:09:18 +0900

    [매장상세] 통신 진행

--- app/src/main/java/com/example/booster/data/remote/RemoteDataSourceImpl.kt
+++ app/src/main/java/com/example/booster/data/remote/RemoteDataSourceImpl.kt
@@ -1,8 +1,15 @@
 package com.example.booster.data.remote
 
+import com.example.booster.data.datasource.model.StoreDetailData
 import com.example.booster.data.remote.network.BoosterServiceImpl
+import io.reactivex.Observable
 
 class RemoteDataSourceImpl : RemoteDataSource {
     val api = BoosterServiceImpl.service
+
+    override fun getStoreDetail(storeIdx : Int): Observable<StoreDetailData> =
+        api.getStoreDetail(storeIdx)
+            .map {
+                it }
 
 }
