e1b68cb55454d32be258d94a8a293c00fc9d3182
KCY <ghkdua059@gmail.com>
2020-07-07 22:17:27 +0900

    [상세보기] 원 색깔 바꾸기

--- app/src/main/java/com/example/booster/ui/storeDetail/StoreDetailActivity.kt
+++ app/src/main/java/com/example/booster/ui/storeDetail/StoreDetailActivity.kt
@@ -4,36 +4,29 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.booster.R
 import com.example.booster.databinding.ActivityStoreDetailBinding
 
 class StoreDetailActivity : AppCompatActivity() {
-    lateinit var storeDetailViewModel: StoreDetailViewModel
-
+    private lateinit var storeDetailViewModel: StoreDetailViewModel
     lateinit var storeDetailBinding: ActivityStoreDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         storeDetailBinding = DataBindingUtil.setContentView(this, R.layout.activity_store_detail)
+
         storeDetailViewModel =
             ViewModelProvider(this@StoreDetailActivity).get(StoreDetailViewModel::class.java)
 
         storeDetailBinding.vm = storeDetailViewModel
 
+        storeDetailViewModel.getData()
+
         val position = intent.getIntExtra("position", -1)
 
         Toast.makeText(this@StoreDetailActivity, "$position", Toast.LENGTH_SHORT).show()
 
-//        getObserved()
     }
 
-    fun getObserved() {
-//        storeDetailViewModel.viewModel.observe(this, Observer {
-//            storeDetailBinding.vm =it
-//        })
-//        storeDetailViewModel.subway.observe(this, Observer {
-//            storeDetailBinding.subway = it
-//        })
-    }
 }
