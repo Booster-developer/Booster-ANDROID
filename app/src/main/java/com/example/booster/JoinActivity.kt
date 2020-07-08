fb84582185d3ec22532a57f88e40d657c1d3114d
leejeongmin <danmin20@khu.ac.kr>
2020-07-08 21:17:30 +0900

    [회원가입/로그인] UI 구현

--- app/src/main/java/com/example/booster/JoinActivity.kt
+++ app/src/main/java/com/example/booster/JoinActivity.kt
@@ -2,11 +2,32 @@ package com.example.booster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import kotlinx.android.synthetic.main.activity_join.*
 
 class JoinActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_join)
+
+        univSelectBtn.setOnClickListener {
+            Log.d("eee",univSelected.text.toString())
+            if (univSelected.text == ""){
+                Log.d("eee","bbbbb")
+                univList.visibility = View.VISIBLE
+            }
+        }
+
+        univ_1.setOnClickListener {
+            univSelected.text = "숭실대학교"
+        }
+        univ_2.setOnClickListener {
+            univSelected.text = "중앙대학교"
+        }
+        univ_3.setOnClickListener {
+            univSelected.text = "서울대학교"
+        }
     }
 }
--- app/src/main/java/com/example/booster/LoginActivity.kt
+++ app/src/main/java/com/example/booster/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.example.booster
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        goto_join.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivityForResult(intent, 100)
+        }
 
     }
 }
