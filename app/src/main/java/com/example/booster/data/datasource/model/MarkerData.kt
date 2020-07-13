fc4964a9a5489720b2554c2542f9da6dca4897c4
JiHyeonMon <jhk8806@naver.com>
2020-07-13 23:19:27 +0900

    [매장리스트] 학교 다이얼로그 수정중

--- app/src/main/java/com/example/booster/data/datasource/model/MarkerData.kt
+++ app/src/main/java/com/example/booster/data/datasource/model/MarkerData.kt
@@ -1,7 +1,11 @@
 package com.example.booster.data.datasource.model
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class MarkerData(
     val latitude: Double,
     val longitude: Double,
-    val data: String
-)
+    val name: String?
+) : Parcelable
