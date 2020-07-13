dae9db3778a71205d2632801145d7c697c1fef4f
leejeongmin <danmin20@khu.ac.kr>
2020-07-14 00:50:38 +0900

    [마이페이지] 뷰 구현

--- app/src/main/java/com/example/booster/ui/myPage/EditProfileFragment.kt
+++ app/src/main/java/com/example/booster/ui/myPage/EditProfileFragment.kt
@@ -7,16 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.booster.R
-import kotlinx.android.synthetic.main.fragment_mypage.*
 
-
-class MypageFragment : Fragment() {
+class EditProfileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_mypage, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
 }
