fcfcfb942fd6d4bb72c5da2c44a51e5e062d0e68
yejin <jinee0717@daum.net>
2020-07-15 06:06:10 +0900

    [옵션설정] 통신 수정

--- app/src/main/java/com/example/booster/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/booster/ui/home/HomeFragment.kt
@@ -4,13 +4,10 @@ import android.animation.Animator
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.os.Handler
-import androidx.lifecycle.ViewModelProviders
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.airbnb.lottie.LottieAnimationView
 
 import com.example.booster.R
 import kotlinx.android.synthetic.main.fragment_home.*
