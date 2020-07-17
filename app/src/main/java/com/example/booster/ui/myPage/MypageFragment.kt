bfe5a9b695c769a11707ba3c26e304e716638163
JiHyeonMon <jhk8806@naver.com>
2020-07-17 09:42:21 +0900

    지현푸시

--- app/src/main/java/com/example/booster/ui/myPage/MypageFragment.kt
+++ app/src/main/java/com/example/booster/ui/myPage/MypageFragment.kt
@@ -10,6 +10,7 @@ import android.view.ViewGroup
 import com.example.booster.R
 import com.example.booster.data.datasource.model.ProfileData
 import com.example.booster.data.remote.network.BoosterServiceImpl
+import com.example.booster.util.UserManager
 import kotlinx.android.synthetic.main.fragment_mypage.*
 import retrofit2.Call
 import retrofit2.Callback
@@ -24,7 +25,9 @@ class MypageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
 
-        BoosterServiceImpl.service.getMyProfile(token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkeCI6MSwiaWF0IjoxNTk0MDI1NzE2LCJleHAiOjE1OTc2MjU3MTYsImlzcyI6IkJvb3N0ZXIifQ.FtWfnt4rlyYH9ZV3TyOjLZXOkeR7ya96afmA0zJqTI8")
+        Log.e("tokentest", UserManager.token)
+
+        BoosterServiceImpl.service.getMyProfile()
             .enqueue(object : Callback<ProfileData> {
                 override fun onFailure(call: Call<ProfileData>, t: Throwable) {
                     Log.e("error", t.toString())
@@ -45,27 +48,6 @@ class MypageFragment : Fragment() {
                 }
 
             })
-//        BoosterServiceImpl.service.getMyProfile(token = "")
-//            .enqueue(object : Callback<ProfileData> {
-//                override fun onFailure(call: Call<ProfileData>, t: Throwable) {
-//                    Log.e("error", t.toString())
-//                }
-//
-//                override fun onResponse(call: Call<ProfileData>, response: Response<ProfileData>) {
-//                    if (response.isSuccessful) {
-//                        val data = response.body()!!.data
-//                        mypage_tv_name.text = data.user_name
-//                        when (data.univ_idx) {
-//                            1 -> mypage_tv_univ.text = "숭실대학교"
-//                            2 -> mypage_tv_univ.text = "중앙대학교"
-//                            3 -> mypage_tv_univ.text = "서울대학교"
-//                        }
-//                        mypage_tv_id.text = data.user_id
-//                        mypage_tv_point.text = data.user_point.toString()
-//                    }
-//                }
-//
-//            })
 
         return inflater.inflate(R.layout.fragment_mypage, container, false)
     }
